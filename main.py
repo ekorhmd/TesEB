062aa79f321c12b385ec08c5759d5a57a6dbedbe
ekorhmd <95470073+ekorhmd@users.noreply.github.com>
2021-12-03 16:38:34 +0700

    no.1
    fibonacci no.1

--- main.py
+++ main.py
@@ -1,41 +1,16 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import mysql.connector
+f0=0
+f1=1
+n=6
 
-url = [
-    'https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl'
-]
+for i in range(10):
+    print(f1, end=' ')
+    n = f1-1+f0+(f1*1)
+    f0 = f1
+    f1 = n
 
-def main():
-    driver = webdriver.chrome()
-    driver.get('https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl'.format(url))
-    content = driver.page_source.encode('utf-8').strip()
-    soup = BeautifulSoup(content, 'lxml')
-
-    id = soup.findAll('a', id= 'video-title')
-    titles = soup.findAll('a', id= 'video-title')
-    name = soup.findAll('a', id= 'video-title')
-    publish = soup.findAll('span', class_='style-scope ytd-video-meta-block')
-    print('Channel: https://youtube.com/{}'.format(url))
-
-    i = 0
-
-    for title in titles[:20]:
-        print('\n{}\t{}\t{}\t{}\t{}'.format(title.text, id[i].text, publish[i].text, name[i].text))
-        i+=1
-    main()
-
-    # koneksi ke database
-    db = mysql.connector.connect(user="root",database="cobaapi")
-    cursor = db.cursor()
-
-    add_new = ("INSERT INTO NO_2"
-               "(channelId, title, channelNama, publishedAt)"
-               "VALUE (%s,%s,%s,%s)")
-    data_news = (id, title, name, publish, 'Fix')
-
-    cursor.excute(add_new,data_news)
-
-    db.commit()
-    cursor.close()
-    db.close()
+    # Untuk Fibonacci sendiri saya belum menemukan solusinya, yang saya pelajari di Fibonacci sendiri yaitu angka sebelumnya akan ditambah angka sekarang misalnya
+    # deret ke - 5 = 3
+    # deret ke - 6 = 5
+    # deret ke - 7 = 3 + 5 = 8
+    # deret ke - 8 = 5 + 8 = 13
+    # kurang lebih saya menangkapnya seperti itu, tapi untuk soal yg kakak kasih ke saya masih bingung bagaimana dapat melompat ke 5 lalu 27
